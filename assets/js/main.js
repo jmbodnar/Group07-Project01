8a168a29aa4a1ecb81bc563b546c7625469e6e8c
Jon Bodnar <jon.bodnar@gmail.com>
2019-10-05 15:08:12 -0400

    Add form elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,62 @@
 var latitude;
 var longitude;
 
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position);
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+  });
+  /* geolocation is available */
+} else {
+  // example http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500
+  $.ajax(
+    'https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPLhCje36DnsPrbL6oI4rXEVFDlZDB6HxP&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+  ).then(function(data) {
+    console.log(data);
+  });
 
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition((position)=>
-    {console.log(position)
-        latitude=position.coords.latitude
-        longitude=position.coords.longitude
-    })
-    /* geolocation is available */
-  } else {
-      // example http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500
-      $.ajax("https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPLhCje36DnsPrbL6oI4rXEVFDlZDB6HxP&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500").then(function(data){
-          console.log(data)
-      })
-      
-      /* geolocation IS NOT available */
-      // render the city/state input
-    }
-                  $.ajax("https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPL&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500").then(function(data){
-                      console.log("ajax",data.results[0].locations[0].latLng)
-                      let latLng= data.results[0].locations[0].latLng
-                      latitude= latLng.lat
-                      longitude= latLng.lng
+  /* geolocation IS NOT available */
+  // render the city/state input
+}
+$.ajax(
+  'https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPL&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
+).then(function(data) {
+  console.log('ajax', data.results[0].locations[0].latLng);
+  let latLng = data.results[0].locations[0].latLng;
+  latitude = latLng.lat;
+  longitude = latLng.lng;
 
-                      $('body').append(`<h1>${latitude}</h1><h2>${longitude}</h2>`)
-                  })
+  $('body').append(`<h1>${latitude}</h1><h2>${longitude}</h2>`);
+});
 
-
-                //   added w3 schools code 
-                  var x = document.getElementById("lat");
+//   added w3 schools code
+var x = document.getElementById('lat');
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
+  } else {
     // x.innerHTML = "Geolocation is not supported by this browser.";
-    console.log("hello");
+    console.log('hello');
   }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-  console.log(position.coords.latitude)
+  x.innerHTML =
+    'Latitude: ' +
+    position.coords.latitude +
+    '<br>Longitude: ' +
+    position.coords.longitude;
+  console.log(position.coords.latitude);
   console.log(position.coords.longitude);
-
 }
 getLocation();
 
 // This section is for "onclick" functions.**************
 
-$("#userSubmit").on("click" , function(){
-
-
+$('#userSubmit').on('click', function() {});
+$('#geoSubmit').on('click', function() {
+  getLocation();
 });
-$("#geoSubmit").on("click" , function(){
-getLocation();
-
-});
-$("#chuck").on("click" , function(){
-
-
-});
-
-
-
-
-
- 
-                  
-
-                 
+$('#chuck').on('click', function() {});
