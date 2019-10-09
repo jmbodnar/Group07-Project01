aeac60e6b7f12f8a97ae91de183945ed502ced18
Jon Bodnar <jon.bodnar@gmail.com>
2019-10-08 20:54:28 -0400

    Temp function, form added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,9 @@
 //***********************************V A R I A B L E S***************************************************************** */
 
-
-
 var latitude;
 var longitude;
 
-
-
 //********************************************E N D _ _ _ O F _ _ _ V A R I A B L E S********************************** */
-
-
-
 
 // ************************************G E O L O C A T I O N _ _ _ C O D E *******************************************************
 if ('geolocation' in navigator) {
@@ -24,7 +17,7 @@ if ('geolocation' in navigator) {
   // example http://open.mapquestapi.com/geocoding/v1/address?key=KEY&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500
   $.ajax(
     'https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPLhCje36DnsPrbL6oI4rXEVFDlZDB6HxP&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
-  ).then(function (data) {
+  ).then(function(data) {
     console.log(data);
   });
 
@@ -32,19 +25,13 @@ if ('geolocation' in navigator) {
   // render the city/state input
 }
 
-
 // **************************************E N D _ _ _ O F _ _ G E O L O C A T I O N _ _ C O D E*****************************************
-
-
-
-
-
 
 //*********************************M A P Q U E S T _ _ A P I _ _ C O D E ************************************************** */
 
 $.ajax(
   'https://open.mapquestapi.com/geocoding/v1/address?key=hCje36DnsPrbL6oI4rXEVFDlZDB6HxPL&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500'
-).then(function (data) {
+).then(function(data) {
   console.log('ajax', data.results[0].locations[0].latLng);
   let latLng = data.results[0].locations[0].latLng;
   latitude = latLng.lat;
@@ -76,35 +63,30 @@ function showPosition(position) {
 }
 getLocation();
 
-
-
 // *******************************************D R I N K _ _ A P I _ _ C O D E ****************************************************
-
 
 // var mildIngredient = ["grape", "lime", "lemon", "orange"];
 // var coldIngredient = ["cinnamon", "cider", "coffee", "vanilla"];
 // var hotIngredient = ["mango", "pineapple", "strawberries", "banana"];
 // var ingredient = apple;
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=pineapple";
+var queryURL =
+  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=pineapple';
 
 $.ajax({
   url: queryURL,
-  method: "GET"
-})
+  method: 'GET'
+}).then(function(response) {
+  console.log(response);
+  var results = response.drinks;
+  // console.log(results);
 
-  .then(function (response) {
-    console.log(response)
-    var results = response.drinks
-    // console.log(results);
+  var htmlString = '';
 
-    var htmlString = '';
+  for (let d = 0; d < results.length; d++) {
+    var drinkName = results[d].strDrink;
+    var drinkImage = results[d].strDrinkThumb;
 
-    for (let d = 0; d < results.length; d++) {
-      var drinkName = results[d].strDrink;
-      var drinkImage = results[d].strDrinkThumb;
-
-
-      htmlString += `
+    htmlString += `
         <div class="drink-card">
         <div class="drink-card__header">${drinkName}</div>
         <div class="drink-card__body">
@@ -119,31 +101,52 @@ $.ajax({
         </div>
       </div>
         `;
-    }
+  }
 
-    $(".drinks").html(htmlString);
+  $('.drinks').html(htmlString);
 
+  // var nameOfDrink = $("<h3>").text(drinkName)
+  // $(".drink-card__header").append(nameOfDrink);
 
+  // var image = $("<img>");
+  // image.attr("src", results[d].strDrinkThumb);
 
-
-
-    // var nameOfDrink = $("<h3>").text(drinkName)
-    // $(".drink-card__header").append(nameOfDrink);
-
-    // var image = $("<img>");
-    // image.attr("src", results[d].strDrinkThumb);
-
-    // $(".drink-image").append(image);
-  });
-
-
+  // $(".drink-image").append(image);
+});
 
 // ********************************************O N - C L I C K _ _ F U N C T I O N S
 
-$('#userSubmit').on('click', function () { });
-$('#geoSubmit').on('click', function () {
+$('#userSubmit').on('click', function() {});
+$('#geoSubmit').on('click', function() {
   getLocation();
 });
-$('#chuck').on('click', function () { });
+$('#chuck').on('click', function() {});
 
+// ===== Jon's Form Stuff ===== //
 
+// ----- Functions ----- //
+
+function getCityWeather(event) {
+  var city = event.target.city.value.trim().toLowerCase();
+
+  if (!city) {
+    alert('You need a city');
+    return;
+  }
+
+  var queryString = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9d43fd3ba9edb19355aa0760b84ba2f2`;
+
+  $.ajax({
+    url: queryString,
+    method: 'GET'
+  }).then(function(response) {
+    var c = response.main.temp;
+    var f = Math.round((c - 273.15) * 1.8 + 32);
+    console.log('Fahrenheit in', city, 'is', f);
+    return f;
+  });
+}
+
+// ----- Listeners and Initializations ----- //
+
+document.addEventListener('submit', getCityWeather, false);
